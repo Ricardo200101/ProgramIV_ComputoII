1a583b005f9d5bf53b80d6ff72b35780a8a7bf2b
Ricardo200101 <87873836+Ricardo200101@users.noreply.github.com>
2023-03-08 21:11:55 -0600

    Week 08

---
+++ phpfiles/index.php
@@ -0,0 +1,14 @@
+<?php
+   echo phpinfo(); 
+    $variable = "Programacion IV";
+    $entero = "0";
+
+    var_dump($variable); 
+
+    // Casting 
+    $variable = (bool) $variable; 
+
+    //var_dump($variable)
+
+    //??
+?>
